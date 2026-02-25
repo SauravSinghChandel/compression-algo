a22df96a3755cb21cf8c24d97ef252d8ea3027dc
SauravSinghChandel <sauravca527@gmail.com>
2026-02-25 03:14:41 -0330

    Added bit -> bytes packing and bytes -> bits unpacking

--- huffman.py
+++ huffman.py
@@ -2,7 +2,28 @@ import heapq
 from collections import Counter
 
 
-def huffmanEncode(text):
+def pack_bits(bitstring: str):
+    pad_bits = (-len(bitstring) % 8)
+    bitstring_padded = bitstring + ("0" * pad_bits)
+
+    out = bytearray()
+    for i in range(0, len(bitstring_padded), 8):
+        byte_str = bitstring_padded[i:i+8]
+        out.append(int(byte_str, 2))
+
+    return bytes(out), pad_bits
+
+
+def unpack_bits(data: bytes, pad_bits: int):
+    bits = "".join(f"{b:08b}" for b in data)
+
+    if pad_bits:
+        bits = bits[:-pad_bits]
+
+    return bits
+
+
+def huffman_encode(text):
 
     class Node:
         def __init__(self, n, val='internal'):
@@ -56,41 +77,49 @@ def huffmanEncode(text):
     preOrder(root, "")
 
     encoded = "".join(codes[c] for c in text)
+    packed, pad_bits = pack_bits(encoded)
 
-    return encoded, codes, originalLen
+    return packed, codes, originalLen, pad_bits
 
 
-def huffmanDecode(encoded, codes, originalLen=None):
+def huffman_decode(packed, codes, original_len=None, pad_bits=0):
     if not codes:
         return ""
+
+    encoded = unpack_bits(packed, pad_bits)
 
     rev = {bitstr: sym for sym, bitstr in codes.items()}
 
     if len(rev) == 1:
-        sym = next(iter(rev.value()))
-        if originalLen is None:
+        sym = next(iter(rev.values()))
+        if original_len is None:
             return sym * len(encoded)
-        return sym * originalLen
+        return sym * original_len
 
     out = []
+    out_len = 0
     buf = ""
     for bit in encoded:
         buf += bit
         if buf in rev:
             out.append(rev[buf])
+            out_len += 1
             buf = ""
 
-    if buf != "":
+            if original_len and out_len == original_len:
+                break
+
+    if not original_len and buf != "":
         raise ValueError(
             "Invalide encoded bitstream: leftover bits do not form a codeword")
 
     return "".join(out)
 
 
-text = "Saurav singh chandel"
-encoded, codes, og = huffmanEncode(text)
-print(encoded)
+text = "Saurav Singh Chandel"
+packed, codes, og, pad_bits = huffman_encode(text)
+print(packed)
 print("#####################################")
-decoded = huffmanDecode(encoded, codes, og)
+decoded = huffman_decode(packed, codes, og, pad_bits)
 
 print(decoded)
